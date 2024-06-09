6dcad097e10c3d225da09f5d94d7b140963f2cca
Naman Tripathi <synamantripathi@gmail.com>
2024-06-09 13:51:37 +0530

    fixed build file

--- today.py
+++ today.py
@@ -8,7 +8,7 @@ import hashlib
 
 # Personal access token with permissions: read:enterprise, read:org, read:repo_hook, read:user, repo
 HEADERS = {'authorization': 'token '+ os.environ['ACCESS_TOKEN']}
-USER_NAME = 'trippin-naman' # 'Andrew6rant'
+USER_NAME = os.environ['USER_NAME']
 QUERY_COUNT = {'user_getter': 0, 'follower_getter': 0, 'graph_repos_stars': 0, 'recursive_loc': 0, 'graph_commits': 0, 'loc_query': 0}
 
 
